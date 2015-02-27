ecf390df95e0ccf2330f22087e798e11e1503033
zaven777 <zaven777@gmail.com>
2015-02-27 11:49:16 -0800

    Initial commit

---
+++ app/src/main/java/com/example/zaven/gallery/ImageAdapter.java
@@ -0,0 +1,65 @@
+package com.example.zaven.gallery;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.content.Context;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+        return imageView;
+    }
+
+    // references to our images
+    public Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_1,
+            R.drawable.sample_1, R.drawable.sample_2,
+            R.drawable.sample_1, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_2,
+            R.drawable.sample_2, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_2,
+            R.drawable.sample_1, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_2,
+            R.drawable.sample_1, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_2
+    };
+}
---
+++ app/src/main/java/com/example/zaven/gallery/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.zaven.gallery;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.widget.GridView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+
+
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
